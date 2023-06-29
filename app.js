8d7861e071fd6c6b08a48ded79f5d2e6e3188214
Daniel <kkdh1109@gmail.com>
2023-06-29 15:16:31 -0400

    connect to mongodb, create model called article

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
-let express = require('express');
-const path = require('path');
-
+let express = require("express");
+const path = require("path");
+require("dotenv").config();
 const app = express();
+const connectDB = require("./config/db");
+let Article = require("./models/article");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-const port = process.env.PORT||3000;
+connectDB();
 
-app.get('/', (req, res) => {
-    let articles = [
-        {
-            id: 1,
-            title: 'Article 1',
-            author: 'kim',
-            body: ' this is article 1'
-        },
-        {
-            id: 2,
-            title: 'Article 2',
-            author: 'lee',
-            body: ' this is article 2'
-        },
-        {
-            id: 3,
-            title: 'Article 3',
-            author: 'park',
-            body: ' this is article 3'
-        }
-    ]
-    res.render('index', {
-        title: 'Hello',
-        articles
-    });
+const port = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  let articles = await Article.find();
+  res.render("index", {
+    title: "Hello",
+    articles,
+  });
 });
 
-app.get('/articles/add', (req, res) => {
-    res.render('addArticle', {
-        title: 'Add Article'
-    });
-})
+app.get("/articles/add", (req, res) => {
+  res.render("addArticle", {
+    title: "Add Article",
+  });
+});
 
-app.listen(port , () => {
-    console.log(`Server running on ${port}`)
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
