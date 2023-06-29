0e4d19a2b7a854c2a4a385aad3041cf9b123f8e7
Daniel <kkdh1109@gmail.com>
2023-06-29 16:29:23 -0400

    add bootstrap, nav, and route for single article

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ require("dotenv").config();
 const app = express();
 const connectDB = require("./config/db");
 let Article = require("./models/article");
+const bodyParser = require("body-parser");
+const bodyparser = require('body-parser');
+
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 connectDB();
 
-const port = process.env.PORT || 3000;
+//bodyparser middleware 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//home
 app.get("/", async (req, res) => {
   let articles = await Article.find();
   res.render("index", {
@@ -20,11 +30,38 @@ app.get("/", async (req, res) => {
   });
 });
 
+//add article route
 app.get("/articles/add", (req, res) => {
   res.render("addArticle", {
     title: "Add Article",
   });
 });
+
+//get single article
+app.get('/article/:id', async (req, res) => {
+    let article = await Article.findById(req.params.id)
+    console.log(article)
+    res.render("article", {
+        article
+      });
+})
+
+//add article submit POST route
+app.post('/articles/add', (req, res) => {
+    let article = new Article();
+
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save().then(() => {
+        res.redirect('/')
+    }
+    ).catch((e) => {
+        console.log(e);
+        return;
+    })
+})
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
